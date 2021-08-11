8ab54f108ebdafcaebb7ce22093a82c9935c0c2c
akashrajcashrayz <53985845+akashrajcashrayz@users.noreply.github.com>
2021-08-11 09:40:00 +0530

    Add files via upload

---
+++ humanactivity.py
@@ -0,0 +1,54 @@
+from flask import Flask, make_response, request, render_template
+import pandas as pd
+from tensorflow import keras
+import numpy as np
+model = keras.models.load_model('/actionnormal.h5')
+app = Flask(__name__)
+
+def transform(text_file_contents):
+    return text_file_contents.replace("=", ",")
+
+
+@app.route('/')
+def form(prediction_text = 'your predicted activity will be shown here in series'):
+    return """
+        <html>
+            <body>
+                <h1>Human Activity Prediction</h1>
+                <h3>Kindly upload Device Motion recording using SensingKit or Crowd Sense at 50hz for prediction</h3>
+                <form action="/predict" method="post" enctype="multipart/form-data">
+                    <input type="file" name="data_file" />
+                    <input type="submit" />
+                </form>
+                <br><br><h3>{prediction_text}</h3><br>
+                </body>
+            </body>
+        </html>
+    """.format(prediction_text=prediction_text)
+
+
+@app.route('/predict', methods=["POST",'GET'])
+def transform_view():
+  try:
+
+    f = request.files['data_file']
+    if not f:
+        return "No file"
+    motiondata = pd.read_csv(f)
+    seriesin = np.array(motiondata[['attitude.roll'	,'attitude.pitch'	,'attitude.yaw',	'gravity.x'	,'gravity.y'	,'gravity.z'	,'rotationRate.x',	'rotationRate.y',	'rotationRate.z'	,'userAcceleration.x'	,'userAcceleration.y',	'userAcceleration.z']])
+
+    rate = 250
+    reminder  = (len(seriesin))%rate
+    serieslenthtotake = len(seriesin) - reminder
+    seriesinput = np.array([seriesin[-serieslenthtotake:][n:n+rate] for n in range(0, len(seriesin[-serieslenthtotake:]), rate)])
+    predictions = model.predict(seriesinput)
+    max_predictions = np.argmax(predictions, axis=1)
+    my_dict ={1:'downstairs', 4:'jogginf', 0:'sitting', 3:'standing', 2:'upstairs', 5:'waling'}
+    activity_predition = [my_dict[x] for x in max_predictions]
+
+    return (form(prediction_text = activity_predition))
+  except:
+    return (form(prediction_text = 'please check csv file again and try again'))  
+
+if __name__ == "__main__":
+    app.run()
